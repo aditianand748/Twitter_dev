7ceaaf1794f422838d625bb6e31cca3deb1a5b8d
Aditi <aditianand748@gmail.com>
2025-11-26 19:09:39 +0530

    Added services

--- src/index.js
+++ src/index.js
@@ -2,20 +2,16 @@ const express = require('express');
 const connect = require('./config/database');
 const app = express();
 
-const TweetRepository = require('./repository/tweet-repository');
-const Comment = require('./models/comment');
+const Tweet = require('./models/tweet');
 
 app.listen(3000, async() => {
     console.log('server started');
     await connect();
     console.log('Mongo db connected');
-    //const tweet = await Tweet.create({
-     //   content: 'third tweet',
-    //});
-   // const tweets = await Tweet.find({userEmail: 'a@b.com'});
-    const tweetRepo = new TweetRepository();
-   const tweet = await tweetRepo.create({content: 'with hooks now'});
-   console.log(tweet);
-   // const tweet = await tweetRepo.getAll(1, 3); 
-    //console.log(tweet[2].contentWithEmail);
+    const tweets = await Tweet.find({
+        content:  ["First tweet", "my tweet", "12345678"]
+        
+    });
+    console.log(tweets);
+
 });
